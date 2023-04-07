205739f774b027f2fd29882995a040079d96d1e2
Gergana Koleva <gerganakoleva03@schoolmath.eu>
2023-04-07 18:41:03 +0300

    Add Doctor controller

---
+++ HealthcareApp/Controllers/DoctorController.cs
@@ -0,0 +1,121 @@
+﻿using Data.Models;
+using HealthcareApp.Services;
+using HealthcareApp.Services.Interfaces;
+using HealthcareApp.Services.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HealthcareApp.Controllers
+{
+    public class DoctorController : Controller
+    {
+        private readonly IDoctorService _service;
+
+        public DoctorController(IDoctorService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            List<DoctorViewModel> doctors = await _service.GetAllAsync();
+
+            return View(doctors);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Details(string id)
+        {
+            try
+            {
+                DoctorViewModel doctor = await _service.GetByIdAsync(id);
+
+                return View(doctor);
+            }
+            catch (ArgumentException e)
+            {
+                ViewData["Message"] = e.Message;
+
+                return RedirectToAction(nameof(Index));
+            }
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(DoctorViewModel doctor)
+        {
+            try
+            {
+                await _service.CreateAsync(doctor);
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch (Exception)
+            {
+                return View(doctor);
+            }
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Edit(string Id)
+        {
+            var doctor = await _service.GetByIdAsync(Id);
+
+            return View(doctor);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(DoctorViewModel doctor)
+        {
+            try
+            {
+                await _service.UpdateAsync(doctor);
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch (Exception)
+            {
+                return View(doctor);
+            }
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Delete(string id)
+        {
+            try
+            {
+                DoctorViewModel doctor = await _service.GetByIdAsync(id);
+
+                return View(doctor);
+            }
+            catch (ArgumentException e)
+            {
+                ViewData["Message"] = e.Message;
+
+                return RedirectToAction(nameof(Index));
+            }
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmation(string id)
+        {
+            try
+            {
+                await _service.DeleteAsync(id);
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch (ArgumentException e)
+            {
+                ViewData["Message"] = e.Message;
+
+                return RedirectToAction(nameof(Index));
+            }
+        }
+    }
+}
--- HealthcareApp/Data/HealthcareAppDbContext.cs
+++ HealthcareApp/Data/HealthcareAppDbContext.cs
@@ -20,10 +20,6 @@ namespace HealthcareApp.Data
         public DbSet<Attendance> Attendances { get; set; }
         public DbSet<Medication> Medications { get; set; }
 
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            optionsBuilder.UseSqlServer("Server=DESKTOP-V8J7KQR\\SQLEXPRESS;Database=HealthcareDB;Trusted_Connection=True;TrustServerCertificate=True;");
-        }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
